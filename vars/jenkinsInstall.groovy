0447869b918df49b3498f31d6e9660ccaa0c3343
Subinay Nag <subinay.nag@gmail.com>
2019-09-20 20:16:39 +0530

    add

---
+++ vars/jenkinsInstall.groovy
@@ -0,0 +1,13 @@
+def playbook(String inventory, String rolePath) {
+    echo "Execute Ansible role..."
+    def shellCommand = "ansible-playbook -i  ${inventory} --role  ${rolePath}"
+    echo "Command is: ${shellCommand}"
+    sh(shellCommand)
+}
+def gitClone(String url) {
+	echo "Executing Git Clone to download Jenkins Installation ansible role"
+	echo "${url}"
+	def shellCommand = "git clone ${url}".execute()
+	echo "Command is: ${shellCommand}"
+    //sh(shellCommand)
+}
