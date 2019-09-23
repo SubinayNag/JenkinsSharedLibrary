3534439c40d00574f97b03f719092f65fdddbf78
Subinay Nag <35738994+SubinayNag@users.noreply.github.com>
2019-09-23 10:17:12 +0530

    Update jenkinsInstall.groovy

--- src/jenkinsInstall.groovy
+++ src/jenkinsInstall.groovy
@@ -1,15 +1,17 @@
 def playbook(String inventory, String rolePath) {
     echo "Execute Ansible role..."
     def path = "pwd"
-	sh(path)
+    //sh(path)
     def shellCommand = "ansible-playbook -i  ${inventory} --role  ${rolePath}"
     echo "Command is: ${shellCommand}"
-    sh(shellCommand)
+    
+    //sh(shellCommand)
 }
 def gitClone(String url) {
 	echo "Executing Git Clone to download Jenkins Installation ansible role"
-	echo "${url}"
+	//echo "${url}"
 	def shellCommand = "git clone --branch=master ${url}"
 	echo "Command is: ${shellCommand}"
-    	sh(shellCommand)
+	shellCommand.execute()
+    	//sh(shellCommand)
 }
